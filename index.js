3a66a480e6ca32495182e697cda3438dfddfbfa0
Derek Rhodes <physci@gmail.com>
2021-06-09 13:46:34 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+
+
+class Segment {
+    constructor(p1, p2) {
+        // connect two points with a conducting element        
+        // currently this is an SVG line
+    }
+    setP1(p) { this.p1 = p; }
+    setP2(p) { this.p2 = p; }
+}
+
+class WireState {
+    constructor() {        
+    }
+}
+
+class WireStateH extends WireState{}
+class WireStateL extends WireState{}
+class WireStateU extends WireState{}
+
+
+class Wire {
+    // the wire on the schematic.
+    constructor(points) {        
+        this.segments = [];
+        // window over points connected each segment
+    }
+}
+
+class ValueWire extends Wire {
+    constructor() {
+        //
+    }
+}
+
+class App {
+    constructor() {
+    }
+}
+
