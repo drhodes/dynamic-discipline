2d76d90dcaa98204dac2a99558e1d3e34075a7fa
Derek Rhodes <physci@gmail.com>
2021-06-17 15:32:38 -0400

    adds first step to tutorial

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import {Term} from './term.js';
 import {err, log} from './err.js';
 import {Waveform} from './waveform.js';
 import {L, H, X} from './sig.js';
+
+
 // -----------------------------------------------------------------------------
 class WireWithLittleNoise {
     constructor() {
@@ -13,7 +15,8 @@ class WireWithLittleNoise {
         let height = 100;        
         let ctx = SVG.SVG().addTo("#wire-with-little-noise").size(width, height);
         let w = new Wire(ctx, 10, 50);
-        // the following done() and init() methods are awful.
+        // the following done() and init() methods are awful, try to
+        // fit them into the constructor.
         w = w.rt(700).done().init().dashed();        
         w.animate();
     }
@@ -54,7 +57,6 @@ class Mux2 {
             .font({family: 'Courier'})
             .move(o.left + SIDE_NUDGE, o.top + VERT_NUDGE_ZERO);
 
-        
         // allocate terminals.
         let inputQ  = new Term(ctx, S, "Q'", o.left-(1*S), o.top+(1*S));
         let inputD  = new Term(ctx, S, 'D', o.left-(1*S), o.top+(3*S)); 
@@ -80,5 +82,25 @@ new Waveform(document.getElementById("wave-id1"),
              50, {});
 
 new Waveform(document.getElementById("wave-id2"),
-             [[0, H], [10, H], [27, L], [30, H], [33, L], [50, H]],
+             [ [0, L],  // start low at time 0.
+               [10, L], // L from 0  -> 10
+               [12, H], // H from 10 -> 12
+               [13, L], // L from 12 -> 13
+               [15, H], // H from 13 -> 15 
+               [18, L],
+               [19, H],
+               [22, L],
+               [500, H],
+             ],
              50, {});
+
+
+
+
+
+new Waveform(document.getElementById("wave-id-step1"),
+             [[0, H], [10, H], [27, L], [30, H], [33, L], [50, H]], 50, {});
+new Waveform(document.getElementById("wave-id-step2"),
+             [[0, H], [10, H], [27, L], [30, H], [33, L], [50, H]], 50, {});
+new Waveform(document.getElementById("wave-id-step3"),
+             [[0, H], [10, H], [27, L], [30, H], [33, L], [50, H]], 50, {});
