b6c88e1a6189ed2f4ac366c403fbd6db114c91a0
Derek Rhodes <physci@gmail.com>
2021-06-19 17:42:14 -0400

    FunctionalSpec gets own module, may not need this class.

---
+++ src/func-spec.js
@@ -0,0 +1,11 @@
+
+class FunctionalSpec {
+    constructor(vil, vol, vih, voh) {
+        // An interactive signal which is optionally associated with
+        // circuit terminal.
+        this.vil = vil;
+        this.vol = vol;
+        this.vih = vih;
+        this.voh = voh;
+    }
+}
