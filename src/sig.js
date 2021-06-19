ee327c756dac64c5d17c0a9bfe3e713cd3a126b1
Derek Rhodes <physci@gmail.com>
2021-06-19 17:39:57 -0400

    sig gets its own module for parsing waveform data

--- src/sig.js
+++ src/sig.js
@@ -1,5 +1,31 @@
+import * as SVG from '@svgdotjs/svg.js';
+import {die, log} from './err.js';
+import {Transition} from './transition.js';
+import {L, H, X} from './transition.js';
+
+export class Sig {
+    // "L 10 H 20 L 20 X 10"
+    constructor(sigString) {        
+        this.transitions = [];        
+        this.parse(sigString);
+    }
+
+    parse(sigString) {
+        let regex = /([L,H,X])\s([0-9]*)/g;        
+        let matches = [...sigString.matchAll(regex)];
+        
+        matches.forEach(m => {
+            let value =
+                m[1] == "L" ? L :
+                m[1] == "H" ? H :
+                m[1] == "X" ? X : die("unrecognized logic value: "+ m[1]);
+                
+            let duration = Number.parseFloat(m[2]);
+            if (Number.isNaN(duration)) die("Couldn't parse: " + m[2] + " as a duration");
+            
+            this.transitions.push(new Transition(duration, value));
+        });
+    }
+}
 
 
-export const L = "LOW";
-export const H = "HIGH";
-export const X = "INVALID";
