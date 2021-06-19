610726bb3a3942ad8d3041c7ddaf380c83fba7e9
Derek Rhodes <physci@gmail.com>
2021-06-19 09:06:40 -0400

    ruminating on a wavegroup container for waveforms

---
+++ src/wavegroup.js
@@ -0,0 +1,17 @@
+import * as SVG from '@svgdotjs/svg.js';
+import {die, log} from './err.js';
+
+class WaveGroup {
+    constructor(div) {
+        // iterate over items in div.
+        // construct waveforms as they encountered.
+
+        this.waveforms = {}; // map from waveform name to waveform
+        
+    }
+
+    
+
+    
+    
+}
