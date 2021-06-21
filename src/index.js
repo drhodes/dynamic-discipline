fb6a963b11755931e1d384adfa2dab7f3a225c7b
Derek Rhodes <physci@gmail.com>
2021-06-21 13:59:07 -0400

    cleans up naming for mux2 to be more consistent.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import {Waveform} from './waveform.js';
 import {WaveGroup} from './wavegroup.js';
 import {Mux2} from './mux2.js';
 
-let mux2 = new Mux2(40);
-mux2.nudgeLabel("outputQ", 1.1, .14);
-mux2.nudgeLabel("inputD", -.5, -.19);
-mux2.nudgeLabel("inputQ", -.5, -.19);
-mux2.nudgeLabel("inputG", .4, .7);
+let mux2 = new Mux2(document.getElementById("mux-2"),  40);
+
+mux2.nudgeLabel("out", 1.1, .14);
+// mux2.nudgeLabel("in1", -.5, -.19);
+// mux2.nudgeLabel("in0", -.5, -.19);
+// mux2.nudgeLabel("sel", .4, .7);
 
 
 new WaveGroup(document.getElementById("wavegroup1"));
--- src/mux2.js
+++ src/mux2.js
@@ -4,19 +4,25 @@ import {Wire} from './wire.js';
 import {Term} from './term.js';
 import {err, log} from './err.js';
 import {Waveform} from './waveform.js';
+import {Attributes} from './attrs.js';
 import {L, H, X} from './sig.js';
 
 
 export class Mux2 {
-    constructor(scale) {
+    constructor(div, scale) {
         log("inits Mux2");
         let width = 700;
-        let height = 300;        
-        let ctx = SVG.SVG().addTo("#mux-2").size(width, height);
-
+        let height = 200;        
+        let ctx = SVG.SVG()
+            .addTo("#mux-2")
+            .size(width, height);
+        
+        // <div id="mux-2" sel="S" in0="D0" in1="D1" out="Q"></div>
+        let attrs = new Attributes(div);
+        
         const S=scale;
         
-        var o = this.origin = {top:height/8 , left:width/2};
+        var o = this.origin = {top:height/100 , left:width/2};
         let icon = new Poly(ctx, o.left, o.top)
             .to(S, S*.75)
             .dn(2.5*S)
@@ -32,7 +38,7 @@ export class Mux2 {
             .font({family: 'Courier'})
             .move(o.left + SIDE_NUDGE,
                   o.top + VERT_NUDGE_ONE);
-
+        
         const VERT_NUDGE_ZERO = S*2.8;
         ctx.text("1")
             .fill("blue")
@@ -41,12 +47,12 @@ export class Mux2 {
 
         // allocate terminals.
         this.terminals = {
-            inputQ: new Term(ctx, S, "Q'", o.left-(1*S), o.top+(1*S)),
-            inputD: new Term(ctx, S, 'D', o.left-(1*S), o.top+(3*S)),
-            inputG:  new Term(ctx, S, 'G', o.left+(0), o.top+(4.1*S)),
-            outputQ: new Term(ctx, S, 'Q', o.left+(1*S), o.top+(2*S)),
-        };
-        this.terminals["inputG"].rotate(-90);
+            in0: new Term(ctx, S, attrs.get("in0"), o.left-(1*S), o.top+(1*S)),
+            in1: new Term(ctx, S, attrs.get("in1"), o.left-(1*S), o.top+(3*S)),
+            sel: new Term(ctx, S, attrs.get("sel"), o.left+(0), o.top+(4.1*S)),
+            out: new Term(ctx, S, attrs.get("out"), o.left+(1*S), o.top+(2*S)),
+        };        
+        this.terminals["sel"].rotate(-90);
         
     }
 
