3730a49480b6bff90775c750762864dffabd66a9
Derek Rhodes <physci@gmail.com>
2021-06-22 18:22:11 -0400

    change mux constructor to suit schematics workflow

--- src/mux2.js
+++ src/mux2.js
@@ -2,27 +2,23 @@ import * as SVG from '@svgdotjs/svg.js';
 import {Poly} from './poly.js';
 import {Wire} from './wire.js';
 import {Term} from './term.js';
-import {err, log} from './err.js';
+import {die, log} from './err.js';
 import {Waveform} from './waveform.js';
 import {Attributes} from './attrs.js';
 import {L, H, X} from './sig.js';
 
 
 export class Mux2 {
-    constructor(div, scale) {
+    constructor(ctx, div, scale, x, y) {
         log("inits Mux2");
-        let width = 700;
-        let height = 200;        
-        let ctx = SVG.SVG()
-            .addTo("#mux-2")
-            .size(width, height);
+        let width = ctx.width();
+        let height = ctx.height();
         
         // <div id="mux-2" sel="S" in0="D0" in1="D1" out="Q"></div>
         let attrs = new Attributes(div);
-        
         const S=scale;
         
-        var o = this.origin = {top:height/100 , left:width/2};
+        var o = this.origin = {top:y , left:x};
         let icon = new Poly(ctx, o.left, o.top)
             .to(S, S*.75)
             .dn(2.5*S)
@@ -32,19 +28,19 @@ export class Mux2 {
             .width(3);
 
         // 
-        const SIDE_NUDGE = S*.35;
-        const VERT_NUDGE_ONE = S*.8;
+        const SIDE_NUDGE = S*.34;
+        const VERT_NUDGE_ZERO = S*.7;
         ctx.text("0")
             .fill("blue")
             .font({family: 'Courier'})
             .move(o.left + SIDE_NUDGE,
-                  o.top + VERT_NUDGE_ONE);
+                  o.top + VERT_NUDGE_ZERO);
         
-        const VERT_NUDGE_ZERO = S*2.8;
+        const VERT_NUDGE_ONE = S*2.7;
         ctx.text("1")
             .fill("blue")
             .font({family: 'Courier'})
-            .move(o.left + SIDE_NUDGE, o.top + VERT_NUDGE_ZERO);
+            .move(o.left + SIDE_NUDGE, o.top + VERT_NUDGE_ONE);
 
         // allocate terminals.
         this.terminals = {
@@ -52,9 +48,13 @@ export class Mux2 {
             in1: new Term(ctx, S, attrs.get("in1"), o.left-(1*S), o.top+(3*S)),
             sel: new Term(ctx, S, attrs.get("sel"), o.left+(0), o.top+(4.1*S)),
             out: new Term(ctx, S, attrs.get("out"), o.left+(1*S), o.top+(2*S)),
-        };        
-        this.terminals["sel"].rotate(-90);
+        };
         
+        this.terminals["sel"].rotate(-90);        
+        this.nudgeLabel("out", .1 , .1);
+        this.nudgeLabel("in1", -.55, .1);
+        this.nudgeLabel("in0", -.55, .1);
+        this.nudgeLabel("sel", 0, .45);
     }
 
     nudgeLabel(termName, dx, dy) {
