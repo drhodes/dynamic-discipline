934142dff20e4adf16c1d7bf0be4541d9970ef14
Derek Rhodes <physci@gmail.com>
2021-06-22 18:11:54 -0400

    make logic values match their digital representations

--- src/transition.js
+++ src/transition.js
@@ -1,8 +1,8 @@
 import {die, log} from './err.js';
 
-export const L = "LOW";
-export const H = "HIGH";
-export const X = "INVALID";
+export const L = "0";
+export const H = "1";
+export const X = "X";
 
 export class Transition {
     constructor(duration, logicValue) { // riseTime? May not need to consider this yet!
