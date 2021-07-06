1ca6f7e5a3a888a4dfe36b438c77e506b35d0f3b
Derek Rhodes <physci@gmail.com>
2021-07-06 17:34:50 -0400

    adds some colors

--- src/color.js
+++ src/color.js
@@ -3,6 +3,10 @@
 export const SCHEM_BLUE = "#3333FFDD";
 export const SCHEM_BLUE_TRANSLUCENT = "#3333FF33";
 
+export const GRAYD = "#DDD";
+export const GRAY5 = "#555";
+export const GRAY9 = "#999";
+export const GRAY3 = "#333";
 export const GRAY5T1 = "#55555511";
 
 export const WIRE_H = "#F00";
