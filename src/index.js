f534c5cbc36ed30e3e0024072b3874eeebcfad68
Derek Rhodes <physci@gmail.com>
2021-07-06 17:35:34 -0400

    integrates slider widget with latch demo

--- src/index.js
+++ src/index.js
@@ -32,18 +32,23 @@ slideATransition();
 
 function latch() {
     let div = document.getElementById("bench3");
-    let bench = new LabBench(div);    
-    let mux2 = bench.schematic.addMux("latch-mux", 350, 50);
+    let bench = new LabBench(div);
+    let slider = bench.widgets[0];
+    
+    const MUX_LEFT = 350;
+    const MUX_TOP = 50;
+    let mux2 = bench.schematic.addMux("latch-mux", MUX_LEFT, MUX_TOP);
     
     mux2.nudgeLabel("Q", 1.3, .37);
     mux2.nudgeLabel("★", -1.9, -.37);
     mux2.nudgeLabel("D", -1.8, -.37);
     mux2.nudgeLabel("G", -.1, .7);
 
-    let p = [410, 110]; // TODO get this value from mux terminal.
+    let p = [MUX_LEFT+60, MUX_TOP+60]; // TODO get this value from mux terminal.
     let wire1 = bench.schematic.addWire("wire1", p[0], p[1]);
     wire1.rt(50).up(75+10).lt(140).dn(45+10).done();
     wire1.init().dashed().animate();
+    wire1.setValue("X");
 
     // TODO this could be made nicer by establishing a "connection" between
     // terminals and wires. maybe later.
@@ -53,55 +58,38 @@ function latch() {
         wire1.setValue(newval);
     });
 
-    let MUX_TPD = 2;
-    let MUX_TCD = 1;
-    
     bench.specialSigfunc("Q", function(wavegroup, t) {
+        let MUX_TPD = slider.value;
+        let MUX_TCD = 1;
+        
         let Q = wavegroup.getWaveform("Q");
         let D = wavegroup.getWaveform("D");
         let G = wavegroup.getWaveform("G");
         
         let clkEdge = G.transitions[1].t;
-        let setupTime = 2 * MUX_TPD;
+        let setupTime = 2 * MUX_TPD; //MUX_TPD;
         let observedSetupTime = clkEdge - D.transitions[2].adjTime();
         let setupTimeMet = observedSetupTime >= setupTime;
-
+        
         let holdTime = MUX_TPD;
         let observedHoldTime = D.transitions[3].adjTime() - clkEdge;
         let holdTimeMet = observedHoldTime >= holdTime;
         
         if (setupTimeMet && holdTimeMet) {
             if (t > (D.transitions[2].adjTime() + MUX_TPD)) {
-                let value = D.sig.valueAtTime(20);
-                return value;
+                return D.sig.valueAtTime(20);
+            } else {
+                return X;
             }
         } else {
-            let valG = G.sig.valueAtTime(t);
+            let valG = G.sig.valueAtTime(t - MUX_TPD);
             if (valG == H) {
                 return D.sig.valueAtTime(t - MUX_TPD);
             } else {
                 return D.sig.valueAtTime(2); // noise
             }
         }
-        return X;
-    });
-    
+    });    
 }
+
 latch();
-
-
-// function BuildFeedback1() {
-//     // this works, but the wires are a bit tedius, a better way?
-//     let schem = new Schematic(document.getElementById("schem1"));
-//     let mux2 = schem.addMux("mux-2", 350, 50);
-
-//     let p = mux2.getConnectionPx("S");
-//     console.log(p);
-
-//     let wire1 = schem.addWire("wire1", p[0], p[1]);
-//     // let wire1 = schem.addWire("wire1", p[0], p[1]);
-//     wire1.rt(50).up(75+10).lt(140).dn(45+10).dashed().done();
-//     wire1.init().dashed().animate();
-
-//     new WaveGroup(document.getElementById("wavegroup1"));
-// }
