8a1aed2aac455e7e3b745512a21ffa4cfab8ee2d
Derek Rhodes <physci@gmail.com>
2021-07-06 19:01:02 -0400

    more work on the "Latching a digital 1" exercise

--- src/color.js
+++ src/color.js
@@ -4,6 +4,7 @@ export const SCHEM_BLUE = "#3333FFDD";
 export const SCHEM_BLUE_TRANSLUCENT = "#3333FF33";
 
 export const GRAYD = "#DDD";
+export const GRAYA = "#AAA";
 export const GRAY5 = "#555";
 export const GRAY9 = "#999";
 export const GRAY3 = "#333";
--- src/slider.js
+++ src/slider.js
@@ -11,7 +11,8 @@ export class Slider {
         let attrs = new Attributes(div);
         this.min = attrs.getAsNum("min");
         this.max = attrs.getAsNum("max");
-        this.value = 0;
+        this.default = attrs.getAsNum("default");
+        this.value = this.default;
         this.ctx.size(700, 30); // TODO get these sizes from parent.
 
         this.labelText = this.ctx
@@ -23,7 +24,7 @@ export class Slider {
         this.setup();
         
         this.numberText = this.ctx
-            .text("0.00ns")
+            .text(this.value + " ns")
             .fill(color.GRAY3)
             .font({family: SCM_FONT, size: 16 })
             .move(122, 4);
@@ -36,17 +37,49 @@ export class Slider {
             add.stop(0, '#fff');
             add.stop(1, '#ddd');
         });
-        gradient.rotate(90);
-        
-        this.rect = this.ctx.rect(200, 20)
-            .move(120, 3)
-            .fill(gradient)
-            .stroke({width: 1, color: "#AAA"});
-        this.knob = this.ctx.rect(18, 18)
-            .move(121, 4)
-            .fill(color.GRAYD)
-            .stroke({width: 1, color: "#AAA"});
+        gradient.rotate(90); 
 
+        const BOUNDS_WIDTH = 200;
+        const BOUNDS_HEIGHT = 20;
+        const BOUNDS_LEFT = 120;
+        const BOUNDS_TOP = 3;
+
+        const BORDER = 1;
+        
+        const KNOB_WIDTH = BOUNDS_HEIGHT- 2*BORDER;
+        const KNOB_HEIGHT = BOUNDS_HEIGHT - 2*BORDER;
+        const KNOB_LEFT = BOUNDS_LEFT + BORDER;
+        const KNOB_RIGHT = BOUNDS_LEFT + BOUNDS_WIDTH - BORDER - KNOB_WIDTH;
+        const KNOB_TOP = BOUNDS_TOP + BORDER;
+        
+        const KNOB_TRAVEL_SPAN = KNOB_RIGHT - KNOB_LEFT;
+
+        // translate slider values to pixel value
+        let valueToPx = v => {
+            let valueSpan = this.max - this.min;
+            let pixelSpan = KNOB_TRAVEL_SPAN;
+
+            let pixelsPerValue = pixelSpan / valueSpan;
+            let valuePixelSpan = pixelsPerValue * v;
+
+            return KNOB_LEFT + valuePixelSpan;
+        };
+
+        // draw the slider background
+        this.rect = this.ctx.rect(BOUNDS_WIDTH, BOUNDS_HEIGHT)
+            .move(BOUNDS_LEFT, BOUNDS_TOP)
+            .fill(gradient)
+            .stroke({width: 1, color: color.GRAYA});
+
+        let knobX = valueToPx(this.default);
+        
+        // draw the slider knob
+        this.knob = this.ctx.rect(KNOB_WIDTH, KNOB_HEIGHT)
+            .move(knobX, KNOB_TOP)
+            .fill(color.GRAYD)
+            .stroke({width: 1, color: color.GRAYA});
+        
+        // deal with events
         var dragging=false;
         this.ctx.mouseleave(function() { dragging=false; });
         this.ctx.mouseup(function() { dragging=false; });
@@ -54,24 +87,23 @@ export class Slider {
         let heel = ev => {
             let newx = ev.layerX-9;
             this.knob.move(newx, 4);
-            let v = (this.max-this.min) * ((newx-120)/182).toFixed(2);
+            let v = (this.max-this.min) * ((newx-BOUNDS_LEFT)/(KNOB_TRAVEL_SPAN)).toFixed(2);
             this.setValue(v);
         };
 
-        let outOfBoundsRight = ev => ev.layerX >= 320-9;
-        let outOfBoundsLeft = ev => ev.layerX <= 120+9;
+        let outOfBoundsRight = ev => ev.layerX >= KNOB_RIGHT + KNOB_WIDTH/2;
+        let outOfBoundsLeft = ev => ev.layerX <= BOUNDS_LEFT+KNOB_WIDTH/2;
         let inbounds = ev => !outOfBoundsLeft(ev) && !outOfBoundsRight(ev);
 
         this.ctx.mousedown(function(ev) {
             dragging=true;
             if (inbounds(ev)) heel(ev); 
         });
-
         
         this.ctx.mousemove(ev => {
             if (dragging && inbounds(ev)) heel(ev);
-            if (dragging && outOfBoundsLeft(ev)) { this.knob.move(120+1); }
-            if (dragging && outOfBoundsRight(ev)) { this.knob.move(320-19); }
+            if (dragging && outOfBoundsLeft(ev)) { this.knob.move(BOUNDS_LEFT+BORDER); }
+            if (dragging && outOfBoundsRight(ev)) { this.knob.move(KNOB_RIGHT); }
         });
     }
 
